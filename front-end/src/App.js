1bd503bf3e7ed5ef205bcedbe64a1aae36af9bb8
larisajz <rl4737@nyu.edu>
2025-03-10 23:12:23 -0400

    revise the logo path

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -11,7 +11,7 @@ function App() {
       <MainPage />
 
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="logo.svg" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
