c1a07b29fa6b979bde4167572cd5830ba74dbda5
ChuqiaoHuang <chuqiao575@gmail.com>
2025-03-15 14:38:44 -0400

    refine calendar daily

--- front-end/src/pages/Calendar/Calendar.js
+++ front-end/src/pages/Calendar/Calendar.js
@@ -8,6 +8,16 @@ import "./Calendar.css"; // Import shared styles
 
 const Calendar = () => {
   const [view, setView] = useState("daily"); // Manage current view
+  const [selectedDate, setSelectedDate] = useState("20 Feb"); //Default select day
+
+  const toDoList = [
+    { date: "20 Feb", toDo: "Interview with A", time: "14:00", TimeRange: "15min" },
+    { date: "26 Feb", toDo: "Lunch with B", time: "12:30", TimeRange: "45min" },
+    { date: "20 Feb", toDo: "Evening Study", time: "18:00", TimeRange: "2h" },
+    { date: "23 Feb", toDo: "Course A HW", time: "20:00", TimeRange: "1h" },
+    { date: "23 Feb", toDo: "Gym Session", time: "07:30", TimeRange: "1h" },
+    { date: "26 Feb", toDo: "Course B", time: "16:00", TimeRange: "2h 15min" },
+  ];
 
   return (
     <div className="calendar-container"> {/* Added wrapper div */}
@@ -35,11 +45,23 @@ const Calendar = () => {
         </button>
       </div>
 
+      {/* Date Selector (Only for Daily View) */}
+      {view === "daily" && (
+        <div className="date-selector">
+          <label>Select Date: </label>
+          <select onChange={(e) => setSelectedDate(e.target.value)} value={selectedDate}>
+            {[...new Set(toDoList.map(toDo => toDo.date))].map(date => (
+              <option key={date} value={date}>{date}</option>
+            ))}
+          </select>
+        </div>
+      )}
+
       {/* Render the selected view */}
       <div className="calendar-view">
-        {view === "daily" && <CalendarDayView />}
-        {view === "weekly" && <CalendarWeekView />}
-        {view === "monthly" && <CalendarMonthView />}
+        {view === "daily" && <CalendarDayView selectedDate={selectedDate} toDoList={toDoList}/>}
+        {view === "weekly" && <CalendarWeekView toDoList={toDoList}/>}
+        {view === "monthly" && <CalendarMonthView toDoList={toDoList}/>}
       </div>
 
       {/* Bottom Navigation */}
